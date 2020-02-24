a092bc501eb3f2016796e80483a49e70d5cea5b5
pholohan <holohan.paul@gmail.com>
2020-02-24 20:23:07 +0000

    Added CRUD and User Validation

---
+++ app/controllers/stadiums.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const Stadium = require('../models/stadium');
+const User = require('../models/user');
+
+const Stadiums = {
+    home: {
+        handler: function(request, h) {
+            return h.view('home', { title: 'Add A Stadium' });
+        }
+    },
+    report: {
+        handler: async function(request, h) {
+            const id = request.auth.credentials.id;
+            const user = await User.findById(id);
+            const stadiums = await Stadium.find({contributer:user}).populate('contributer').lean();
+            return h.view('report', {
+                title: 'Stadiums Added to Date',
+                stadiums: stadiums,
+            });
+        }
+    },
+
+    contribute: {
+        handler: async function(request, h) {
+            const data = request.payload;
+            const id = request.auth.credentials.id;
+            const user = await User.findById(id);
+            const newStadium = new Stadium({
+                name: data.name,
+                county: data.county,
+                capacity: data.capacity,
+                province: data.province,
+                contributer: user._id
+            })
+            await newStadium.save();
+            return h.redirect('/report');
+        }
+    },
+
+    showStadium: {
+        handler: async function(request, h) {
+            try {
+                const id = request.params.id;
+                const stadium = await Stadium.findById(id).lean();
+                return h.view('editstadium', {title: 'Edit Stadium Details', stadium: stadium});
+            }catch(err){
+                return h.view('login', { errors: [{ message: err.message }] });
+            }
+        }
+    },
+
+    updateStadium: {
+        handler: async function(request, h) {
+            try {
+                const stadiumEdit = request.payload;
+                const id = request.params.id;
+                const stadium = await Stadium.findById(id);
+                stadium.name = stadiumEdit.name;
+                stadium.capacity = stadiumEdit.capacity;
+                stadium.county = stadiumEdit.county;
+                stadium.province = stadiumEdit.province;
+                await stadium.save();
+                return h.redirect('/report');
+            }catch(err){
+                return h.view('main', { errors: [{ message: err.message }] });
+            }
+        }
+    },
+
+    deleteStadium: {
+        handler: async function(request, h) {
+            try {
+                const id = request.params.id;
+                const stadium = await Stadium.findById(id).lean();
+                await Stadium.deleteOne(stadium);
+                return h.redirect('/report');
+            }catch(err){
+                return h.view('main', { errors: [{ message: err.message }] });
+            }
+        }
+    },
+
+};
+
+module.exports = Stadiums;
---
+++ routes.js
@@ -0,0 +1,30 @@
+const Accounts = require('./app/controllers/accounts');
+const Stadiums = require('./app/controllers/stadiums');
+
+module.exports = [
+    { method: 'GET', path: '/', config: Accounts.index },
+    { method: 'GET', path: '/signup', config: Accounts.showSignup },
+    { method: 'GET', path: '/login', config: Accounts.showLogin },
+    { method: 'GET', path: '/logout', config: Accounts.logout },
+    { method: 'POST', path: '/signup', config: Accounts.signup },
+    { method: 'POST', path: '/login', config: Accounts.login },
+    { method: 'POST', path: '/contribute', config: Stadiums.contribute },
+    { method: 'GET', path: '/settings', config: Accounts.showSettings },
+    { method: 'POST', path: '/settings', config: Accounts.updateSettings },
+    { method: 'GET', path: '/editstadium/{id}', config: Stadiums.showStadium },
+    { method: 'POST', path: '/editstadium/{id}', config: Stadiums.updateStadium },
+    { method: 'GET', path: '/deletestadium/{id}', config: Stadiums.deleteStadium },
+    { method: 'GET', path: '/home', config: Stadiums.home },
+    { method: 'GET', path: '/report', config: Stadiums.report },
+
+    {
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: './public'
+            }
+        },
+        options: {auth: false}
+    }
+];
