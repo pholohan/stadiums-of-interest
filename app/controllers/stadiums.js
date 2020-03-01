a7d3a02a9440c3e881559b0d9c03386a08e6a78f
pholohan <holohan.paul@gmail.com>
2020-03-01 20:25:12 +0000

    Initial Commiit in CloudAtlas Feature

--- app/controllers/stadiums.js
+++ app/controllers/stadiums.js
@@ -89,7 +89,35 @@ const Stadiums = {
             return h.view('stadiumupload', { title: 'Upload A Stadium Image',stadium: stadium });
         }
     },
-
+    uploadFile: {
+        handler: async function(request, h) {
+            try {
+                const id = request.params.id;
+                const imagename = './public/' + id;
+                const file = request.payload.imagefile;
+                const stadium = await Stadium.findById(id).lean();
+                if (Object.keys(file).length > 0) {
+                    const test = await ImageStore.uploadImage(imagename, request.payload.imagefile);
+                    console.log(test);
+                    stadium.stadiumURL = test;
+                    await Stadium.findOneAndUpdate({_id: id}, {stadiumURL: test});
+                    return h.redirect('/report');
+                }
+                return h.view('gallery', {
+                    title: 'Cloudinary Gallery',
+                    error: 'No file selected'
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        payload: {
+            multipart: true,
+            output: 'data',
+            maxBytes: 209715200,
+            parse: true,
+        }
+    },
 };
 
 module.exports = Stadiums;
